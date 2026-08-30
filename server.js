94f6b19a6f06f9ea1a8748e4c67d107c3d2ec3bc
ghostgostado-ctrl <ghostgostado@gmail.com>
2026-08-30 15:36:07 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+{
+  "name": "lemonade-ai-api",
+  "version": "1.0.0",
+  "description": "Lemonade AI API",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^5.1.0"
+  }
+}
